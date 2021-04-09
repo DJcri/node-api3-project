f46fc955de105f9cda668dbed52045efd007cada
DJcri <djcri123@gmail.com>
2021-04-08 21:23:47 -0700

    made deployable to heroku

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 // require your server and launch it
+const dotenv = require("dotenv").config();
 const server = require("./api/server");
+const cors = require("cors");
+const port = process.env.PORT || 9000;
 
-server.listen(5000, () => {
-  console.log("listening on port 5000");
+server.use(cors());
+
+server.use("/api/", (_, res) => {
+  res.json({ data: "API is accounted for" });
+});
+
+server.listen(port, () => {
+  console.log(`listening on port ${port}`);
 });
