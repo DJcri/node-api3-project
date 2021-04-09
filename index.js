7db9f3cbbed84133805d66e7b32d65f9c7e63d19
DJcri <djcri123@gmail.com>
2021-04-08 21:51:31 -0700

    deployment changes

--- index.js
+++ index.js
@@ -2,13 +2,6 @@
 const dotenv = require("dotenv").config();
 const server = require("./api/server");
 const port = process.env.PORT || 9000;
-const path = require("path");
-
-server.use(cors());
-
-server.use("/api/", (_, res) => {
-  res.json({ data: "API is accounted for" });
-});
 
 server.listen(port, () => {
   console.log(`listening on port ${port}`);
