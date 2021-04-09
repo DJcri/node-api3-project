3f8acf724e87a5f372eeca41942e6c3e6a69c9b2
DJcri <djcri123@gmail.com>
2021-04-08 21:55:24 -0700

    more changes

--- api/server.js
+++ api/server.js
@@ -2,22 +2,15 @@ const express = require("express");
 const cors = require("cors");
 const { logger } = require("./middleware/middleware");
 const usersRouter = require("./users/users-router");
-const path = require("path");
 
 const server = express();
 
 server.use(express.json());
 server.use(logger);
-server.use(cors());
-server.use(express.static(path.join(__dirname, "client/build")));
 // remember express by default cannot parse JSON in request bodies
 
 // global middlewares and the user's router need to be connected here
 
 server.use("/api/users", usersRouter);
-
-server.use("/api/", (_, res) => {
-  res.json({ data: "API is accounted for" });
-});
 
 module.exports = server;
